56e8c33bae1456e83a7aa4e71d4dd188f52d52fb
Marko Mamić <marko.mamic0608@gmail.com>
2021-03-12 11:11:08 +0100

    Završen drugi zadatak

--- LV1/Drugi.py
+++ LV1/Drugi.py
@@ -1,6 +1,12 @@
 ocjena = -1.0
-while (ocjena < 0) or (ocjena > 1):
-    ocjena = float(input())
+while ((ocjena < 0.0) or (ocjena > 1.0)):
+    ocjena = float(input("Unesite broj između 0 i 1: "))
 
+def cases(ocjena):
+    if(ocjena>=0.9): print('A')
+    elif(ocjena>=0.8): print('B')
+    elif(ocjena>=0.7): print('C')
+    elif(ocjena>=0.6): print('D')
+    elif(ocjena<0.5): print('F')
 
-    
+cases(ocjena)
