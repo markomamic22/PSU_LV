dbd008957cc53fd59fe01fccd8ff578614a2ed7f
Marko Mamić <marko.mamic0608@gmail.com>
2021-03-12 14:19:36 +0100

    Peti radi* (treba fixati path bug)

--- LV1/Peti.py
+++ LV1/Peti.py
@@ -2,16 +2,24 @@ import os
 import pathlib
 
 
+#D:\GitHub Repos\PSU_LV\LV1\mbox.txt - prava putanja
+#'D:\\GitHub Repos\\PSU_LV\\mbox.txt' - putanja dobivena kroz program
+
+#iz nekog razloga mi ne radi dobivanje patha kroz program - radi ako predam umjesto imena datoteke putanju
 name = input("Unesite ime datoteke:")
 name = os.path.abspath(name)
-fhand = open(name)
+try:
+    fhand = open(name)
+except:
+    exit()
 sum = 0.0
 counter = 0
 
 for line in fhand:
     line =  line.rstrip()
     if line.startswith("X-DSPAM-Confidence:"):
-        number = float(line.find(": "))
+        start = line.find(":")
+        number = float(line[(start+2):])
         sum += number
         counter +=1
 print("Srednja vrijednost je:",float(sum/counter))        
