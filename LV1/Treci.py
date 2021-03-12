56d2fe7b12246aad73fd123e52cea28eff2a3194
Marko Mamić <marko.mamic0608@gmail.com>
2021-03-12 11:27:30 +0100

    Završen treći zadatak

---
+++ LV1/Treci.py
@@ -0,0 +1,7 @@
+sati = int(input('Unesite radne sate:'))
+zarada = int(input("Koliko ste plaćeni po satu:"))
+
+def total_kn(sati,zarada):
+    rez = sati * zarada
+    return rez
+print("Zaradili ste:\n",total_kn(sati, zarada),"kn")
