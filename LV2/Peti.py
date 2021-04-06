2f276f153359aadb3cd69469739f6c9d4a6166c2
Marko Mamić <marko.mamic0608@gmail.com>
2021-04-06 22:24:28 +0200

    Peti - not done

--- LV2/Peti.py
+++ LV2/Peti.py
@@ -1,11 +1,27 @@
 import numpy as np
-import skimage as sk
-from itertools import chain
+import matplotlib.pyplot as plt
 
-def function(pikseli,broj_visina,broj_sirina):
-    crna = np.zeros(((broj_sirina/2),(broj_visina/2)), dtype=int)
-    bijela = 255*np.ones(((broj_sirina/2),(broj_visina/2)), dtype=int)
+def funkcija(pikseli,broj_visina,broj_sirina):
+    crna = np.zeros((pikseli,pikseli))
+    bijela = 255*np.ones((pikseli,pikseli))
 
+
+    img = [[]]
+    for i in range(int(broj_visina/2)):
+        img= np.vstack((crna,bijela))
     
+    plt.figure(1)
+    plt.imshow(img, cmap='gray', vmin=0, vmax=255)
+    plt.show()
 
-    
+funkcija(5,10,10)
+
+
+
+"""import numpy as np
+import matplotlib.pyplot as plt
+check = np.zeros((5, 5))
+check[::2, 1::2] = 1
+check[1::2, ::2] = 1
+plt.matshow(check, cmap='gray')
+plt.show()"""
