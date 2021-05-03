80da430d2ef8b427b60879ea40f3a31890ac418e
Marko Mamić <marko.mamic0608@gmail.com>
2021-05-03 18:05:12 +0200

    Cetvrti - done

--- LV5/primjer_5_2.py
+++ LV5/primjer_5_2.py
@@ -6,28 +6,28 @@ Created on Sun Dec 02 12:08:00 2018
 """
 
 import scipy as sp
+import os
 from sklearn import cluster, datasets
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
-try:
-    face = sp.face(gray=True)
-except AttributeError:
-    from scipy import misc
-    face = misc.face(gray=True)
-    
-X = face.reshape((-1, 1)) # We need an (n_sample, n_feature) array
-k_means = cluster.KMeans(n_clusters=5,n_init=1)
+os.chdir("LV5")
+imageNew = mpimg.imread('example_grayscale.png')
+
+
+X = imageNew.reshape((-1, 1)) # We need an (n_sample, n_feature) array
+k_means = cluster.KMeans(n_clusters=10,n_init=1)
 k_means.fit(X) 
 values = k_means.cluster_centers_.squeeze()
 labels = k_means.labels_
-face_compressed = np.choose(labels, values)
-face_compressed.shape = face.shape
+imageNew_compressed = np.choose(labels, values)
+imageNew_compressed.shape = imageNew.shape
 
 plt.figure(1)
-plt.imshow(face,  cmap='gray')
+plt.imshow(imageNew,  cmap='gray')
 plt.show()
 
 plt.figure(2)
-plt.imshow(face_compressed,  cmap='gray')
+plt.imshow(imageNew_compressed,  cmap='gray')
 plt.show()
