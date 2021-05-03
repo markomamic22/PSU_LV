e69ba11f84011a641c7da7adecc1a3399474b66f
Marko Mamić <marko.mamic0608@gmail.com>
2021-05-03 18:26:32 +0200

    Not done

---
+++ LV5/Peti.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 02 12:08:00 2018
+
+@author: Grbic
+"""
+
+import scipy as sp
+import os
+from sklearn import cluster, datasets
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+os.chdir("LV5")
+imageNew = mpimg.imread('example.png')
+
+
+X = imageNew.reshape((-1, 1)) # We need an (n_sample, n_feature) array
+k_means = cluster.KMeans(n_clusters=2,n_init=1)
+k_means.fit(X) 
+values = k_means.cluster_centers_.squeeze()
+labels = k_means.labels_
+imageNew_compressed = np.choose(labels, values)
+imageNew_compressed.shape = imageNew.shape
+
+plt.figure(1)
+plt.imshow(imageNew)
+plt.show()
+
+plt.figure(2)
+plt.imshow(imageNew_compressed)
+plt.show()
